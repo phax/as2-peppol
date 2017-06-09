5737c69ae03f7aabd85403403b6cd19b818ab5cb
Philip Helger <philip@helger.com>
2017-06-09 10:35:33 +0200

    Added test file

--- src/test/java/com/helger/peppol/as2client/MainAS2TestClient.java
+++ src/test/java/com/helger/peppol/as2client/MainAS2TestClient.java
@@ -292,9 +292,8 @@ public final class MainAS2TestClient
     if (true)
     {
       aReceiver = IF.createParticipantIdentifierWithDefaultScheme ("0088:5050689000018");
-      sTestFilename = "xml/as2-test-at-gov.xml";
+      sTestFilename = "xml/Use Case 1.a_ExampleFile_PEPPOL BIS.xml";
       aSML = ESML.DIGIT_TEST;
-      aDocTypeID = IF.createDocumentTypeIdentifierWithDefaultScheme ("urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:www.cenbii.eu:transaction:biitrns010:ver2.0:extended:urn:www.peppol.eu:bis:peppol4a:ver2.0::2.1");
     }
 
     // Debug outgoing (AS2 message)?
